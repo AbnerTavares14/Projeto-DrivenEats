6e6ba8c44bf1d8f626a69a0caaa1ab78ebbaad47
Abner <abnerdasilvatavares@gmail.com>
2022-01-27 16:22:45 -0300

    Versão 3.1

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function confirmar(){
-    alert("testando");
-}
+
+
+function seleciona_prato() {
+    const opcao1 = document.querySelectorAll(".prato").getElementsByClassName("prato").style.border = 5px solid red;
+    opcao1.classList.toggle("selecionado");
+
+    console.log(opcao1);
+
+} 
+
+
+
+
+//    border: 5px solid #32B72F;
+//    border-radius: 9px;
+
